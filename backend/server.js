68547ba2dbc96d737c9ac0d58a9d16b185afae2d
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2023-11-19 12:42:19 +0500

    ENV added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
