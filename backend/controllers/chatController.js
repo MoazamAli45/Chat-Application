d9fee1f78cb61c8032fd6015864e14728c154540
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2023-11-28 20:52:12 +0500

    Chat API COMPLETED

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -20,9 +20,7 @@ exports.accessChat = catchAsync(async (req, res) => {
       { users: { $elemMatch: { $eq: userId } } },
       { users: { $elemMatch: { $eq: req.user._id } } },
     ],
-  })
-    .populate("users", "-password")
-    .populate("latestMessage");
+  }).populate("users", "-password");
   // console.log("isChat", isChat);
   //
   isChat = await User.populate(isChat, {
@@ -57,4 +55,122 @@ exports.accessChat = catchAsync(async (req, res) => {
       err,
     });
   }
+});
+//    Fatching Chats of one user
+
+exports.fetchChat = catchAsync(async (req, res) => {
+  //     Sorting on the basis of latest message
+  const results = await Chat.find({
+    users: { $elemMatch: { $eq: req.user._id } },
+  })
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password")
+    .populate("latestMessage")
+    .sort({ updatedAt: -1 });
+
+  //   To populate in order to have the latest message sender details
+  const chats = await User.populate(results, {
+    path: "latestMessage.sender",
+    select: "name profilePic email",
+  });
+
+  res.status(200).json({
+    success: true,
+    chats,
+  });
+});
+
+//   Creating Group Chat  where logged In user will also be the member of the group chat
+exports.createGroupChat = catchAsync(async (req, res) => {
+  const { chatName, users } = req.body;
+  if (!chatName || !users) throw new AppError("Please Provide Chat Name", 400);
+  if (users.length < 2)
+    throw new AppError("Please Provide Atleast Two Users", 400);
+
+  const chatData = {
+    chatName,
+    isGroupChat: true,
+    users: [...users, req.user._id],
+    groupAdmin: req.user._id,
+  };
+  const createdChat = await Chat.create(chatData);
+  const fullChat = await Chat.findById(createdChat._id)
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password")
+    .populate("latestMessage");
+  res.status(200).json({
+    success: true,
+    chat: fullChat,
+  });
+});
+exports.renameGroupChat = catchAsync(async (req, res) => {
+  const { chatId, chatName } = req.body;
+  if (!chatId || !chatName)
+    throw new AppError("Please Provide Chat Id and Chat Name", 400);
+  const chat = await Chat.findById(chatId);
+  if (!chat) throw new AppError("No Chat Found", 404);
+  if (chat.groupAdmin.toString() !== req.user._id.toString())
+    throw new AppError("You are not the admin of this group", 403);
+  chat.chatName = chatName;
+  await chat.save();
+  const groupChat = await Chat.findById(chat._id)
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password")
+    .populate("latestMessage");
+
+  res.status(200).json({
+    success: true,
+    groupChat,
+  });
+});
+
+//  Removing User From Group Chat
+exports.removeFromGroup = catchAsync(async (req, res) => {
+  const { chatId, userId } = req.body;
+  const chat = await Chat.findById(chatId);
+  if (!chat) throw new AppError("No Chat Found", 404);
+  if (chat.groupAdmin.toString() !== req.user._id.toString())
+    throw new AppError("You are not the admin of this group", 403);
+
+  if (chat.users.length <= 2)
+    throw new AppError("You can not remove all the users", 403);
+  const index = chat.users.indexOf(userId);
+  if (index > -1) {
+    chat.users.splice(index, 1);
+  }
+  await chat.save();
+  const groupChat = await Chat.findById(chat._id)
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password")
+    .populate("latestMessage");
+
+  res.status(200).json({
+    success: true,
+    groupChat,
+  });
+});
+exports.addMemberToGroup = catchAsync(async (req, res) => {
+  const { chatId, userId } = req.body;
+  const chat = await Chat.findById(chatId);
+  if (!chat) throw new AppError("No Chat Found", 404);
+  if (chat.groupAdmin.toString() !== req.user._id.toString())
+    throw new AppError("You are not the admin of this group", 403);
+  //   Matching if all ready user present then throw error
+  if (chat.users.includes(userId))
+    throw new AppError("User is already present in the group", 403);
+
+  //      RESTRICITNG TO ADD MORE THAN 10 USERS
+  // if (chat.users.length >= 10)
+  //   throw new AppError("You can not add more than 10 users", 403);
+  chat.users.push(userId);
+  await chat.save();
+  const groupChat = await Chat.findById(chat._id)
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password")
+    .populate("latestMessage");
+
+  res.status(200).json({
+    success: true,
+    groupChat,
+  });
 });
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -5,9 +5,9 @@ const { protect } = require("../controllers/authController");
 // router.get("/", chatController.getChat);
 
 router.post("/", protect, chatController.accessChat);
-//  router.get("/",protect,fetchChat);
-// router.post("/",protect,createGroupChat);
-// router.get("/",protect,renameGroup);
-// router.get("/",protect,removeFromGroup);
-// router.get("/",protect,addToMember);
+router.get("/", protect, chatController.fetchChat);
+router.post("/group-chat", protect, chatController.createGroupChat);
+router.put("/group-chat", protect, chatController.renameGroupChat);
+router.delete("/group-chat-member", protect, chatController.removeFromGroup);
+router.post("/group-chat-member", protect, chatController.addMemberToGroup);
 module.exports = router;
