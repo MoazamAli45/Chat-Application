bd8853886cf80b7e4be0c70eaca4645b6a309ffd
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2024-01-06 00:57:55 +0500

    Add MongoDB connection and logging

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ const colors = require("colors");
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
+    //   Correct
     const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
