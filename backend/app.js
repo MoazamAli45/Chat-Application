b7f4029c11156e342ceff4645ce435af8157917a
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2024-01-06 01:04:33 +0500

    Add CORS support for all routes

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
+app.options("*", cors());
 app.get("/", (req, res) => {
   res.send("Welcome to Chat Application!");
 });
--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,10 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingtimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+
+    //   FOR PRODUCTION
+    origin: "*",
     // credentials: true,
   },
 });
